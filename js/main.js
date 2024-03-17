59ff5d2b427224364ace5f86bcd6405368cc1057
Ran <ranglikman@gmail.com>
2024-03-17 21:31:44 +0200

    Delivery1 - Progress Submission

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-"use strict"
+'use strict'
 
 let gCanvas
 let gCtx
@@ -6,8 +6,8 @@ let currentImage = null
 let fontSize = 30
 
 function onInit() {
-    gCanvas = document.querySelector("canvas")
-    gCtx = gCanvas.getContext("2d")
+    gCanvas = document.querySelector('canvas')
+    gCtx = gCanvas.getContext('2d')
     loadImages()
     addTextListener()
 }
@@ -22,37 +22,37 @@ function drawImageAndText() {
         gCtx.clearRect(0, 0, gCanvas.width, gCanvas.height)
         gCtx.drawImage(currentImage, 0, 0, gCanvas.width, gCanvas.height)
     }
-    const text = document.querySelector(".text-input").value
-    const color = document.querySelector(".color-input").value
+    const text = document.querySelector('.text-input').value
+    const color = document.querySelector('.color-input').value
     if (text) {
         gCtx.font = `${fontSize}px Impact`
         gCtx.fillStyle = color
-        gCtx.textAlign = "center"
+        gCtx.textAlign = 'center'
         gCtx.fillText(text, gCanvas.width / 2, gCanvas.height - 50)
         gCtx.lineWidth = fontSize / 20
     }
 }
 
 function onDownloadCanvas(elLink) {
-    const dataUrl = gCanvas.toDataURL("image/png")
+    const dataUrl = gCanvas.toDataURL('image/png')
     elLink.href = dataUrl
-    elLink.download = "my-meme.png"
+    elLink.download = 'my-meme.png'
 }
 
 function addTextListener() {
-    const textInput = document.querySelector(".text-input")
-    textInput.addEventListener("input", drawImageAndText)
+    const textInput = document.querySelector('.text-input')
+    textInput.addEventListener('input', drawImageAndText)
 }
 
 function loadImages() {
-    const imageContainer = document.querySelector(".select-img-container")
+    const imageContainer = document.querySelector('.select-img-container')
     const numberOfImages = 10
 
     for (let i = 1; i <= numberOfImages; i++) {
-        const img = document.createElement("img")
-        img.setAttribute("src", `img/memes/${i}.jpg`)
-        img.setAttribute("title", "Local img")
-        img.setAttribute("onclick", "onSelectImg(this)")
+        const img = document.createElement('img')
+        img.setAttribute('src', `img/memes/${i}.jpg`)
+        img.setAttribute('title', 'Local img')
+        img.setAttribute('onclick', 'onSelectImg(this)')
 
         imageContainer.appendChild(img)
     }
