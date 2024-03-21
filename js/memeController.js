4e3497e0e0ff6c64b962e96876d182eddc152766
Ran <ranglikman@gmail.com>
2024-03-21 21:43:36 +0200

    Delivery4 - Score Submission

--- js/memeController.js
+++ js/memeController.js
@@ -55,3 +55,7 @@ function renderMeme(currentText = '') {
         img.onload = drawContent
     }
 }
+
+/* -------------------------------------------------------------------------- */
+
+// Delivery4
